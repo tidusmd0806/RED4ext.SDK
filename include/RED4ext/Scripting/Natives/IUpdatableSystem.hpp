c584d2276f9d28162d20cc39c105910e3d316252
Jack Humbert <jack.humb@gmail.com>
2022-07-31 16:42:53 -0400

    more methods

--- include/RED4ext/Scripting/Natives/IUpdatableSystem.hpp
+++ include/RED4ext/Scripting/Natives/IUpdatableSystem.hpp
@@ -8,6 +8,7 @@
 
 namespace RED4ext
 {
+
 struct IUpdatableSystem : IScriptable
 {
     static constexpr const char* NAME = "IUpdatableSystem";
@@ -16,7 +17,7 @@ struct IUpdatableSystem : IScriptable
 
     // 1.52 RVA: 0xA86060 / 11034720
     /// @pattern 0F B6 C1 83 F8 0C 77 7B 48 8D 15 91 9F 57 FF 8B 8C 82 EC 60 A8 00 48 03 CA FF E1 48 8D 05 46 18
-    static const char *__fastcall IUpdatableSystem_GetStringForUnk0(char);
+    static const char *__fastcall GetStringForUnk0(Unk0);
 
     // 1.52 RVA: 0xA855E0 / 11032032
     // contains strings for IUpdatableSystem::Unk1
@@ -165,12 +166,57 @@ struct IUpdatableSystem : IScriptable
         char *nameStr;
         uint32_t unk68;
         uint32_t unk6C;
-        uint64_t unk70;
-        uint64_t unk78;
+        void * unk70;
+        uint8_t unk78;
+        uint8_t unk79[7];
+
+        // 1.52 RVA: 0xA86960 / 11037024
+        /// @pattern 48 89 5C 24 18 55 56 57 41 56 41 57 48 83 EC 60 4C 8B F1 48 8B DA 0F B6 4A 32 E8 91 16 13 02 48
+        void FireCallback(void * a2);
+
+        // 1.52 RVA: 0xA85C10 / 11033616
+        /// @pattern 48 89 5C 24 08 48 89 74 24 10 48 89 7C 24 18 41 56 48 83 EC 20 0F B7 02 48 8D 72 08 66 89 01 48
+        UpdateStorage *__fastcall Copy(UpdateStorage *a2);
+
     };
 
     // sub_110 - adds callbacks to the lookup - there are at least 10 dynarrays
     virtual void RegisterCallbacks(DynArray<UpdateStorage>** lookup) { }; 
 };
 RED4EXT_ASSERT_SIZE(IUpdatableSystem, 0x40);
+
+struct UpdateSystem {
+
+    // 1.52 RVA: 0xA85990 / 11032976
+    /// @pattern 48 89 6C 24 10 48 89 74 24 18 48 89 7C 24 20 41 56 48 83 EC 20 8B B1 D0 00 00 00 33 ED 4C 8B F1
+    void __fastcall ReleaseUnknown();
+
+    // 1.52 RVA: 0xA86820 / 11036704
+    /// @pattern 48 89 5C 24 08 48 89 74 24 10 48 89 7C 24 18 41 56 48 83 EC 40 0F B6 1A 48 8B F2 48 C1 E3 04 48
+    uint64_t __fastcall RegisterUpdate(IUpdatableSystem::UpdateStorage *update);
+
+    // 1.52 RVA: 0xA86DE0 / 11038176
+    /// @pattern 48 89 5C 24 10 48 89 6C 24 18 48 89 74 24 20 57 41 54 41 55 41 56 41 57 48 83 EC 20 4C 8B 29 B8
+    __int64 __fastcall IterateUnknown(CClass ***a2, CClass ***a3);
+
+    // 1.52 RVA: 0xA86120 / 11034912
+    /// @pattern 4C 8B DC 4D 89 43 18 49 89 53 10 56 48 81 EC B0 00 00 00 8B 84 24 E0 00 00 00 48 8B F2 44 3B C8
+    void *__fastcall PrepareFiring(__int64 a2, int *a3, unsigned int a4, unsigned int a5);
+
+    DynArray<IUpdatableSystem::UpdateStorage> frameBegin;
+    DynArray<IUpdatableSystem::UpdateStorage> multiplayer_UpdateStateSnapshots; 
+    DynArray<IUpdatableSystem::UpdateStorage> entityUpdateState; 
+    DynArray<IUpdatableSystem::UpdateStorage> preBuckets; 
+    DynArray<IUpdatableSystem::UpdateStorage> buckets; 
+    DynArray<IUpdatableSystem::UpdateStorage> postBuckets; 
+    DynArray<IUpdatableSystem::UpdateStorage> cameraUpdate; 
+    DynArray<IUpdatableSystem::UpdateStorage> playerAimUpdate; 
+    DynArray<IUpdatableSystem::UpdateStorage> postPlayerAimUpdate; 
+    DynArray<IUpdatableSystem::UpdateStorage> mappinsUpdate; 
+    DynArray<IUpdatableSystem::UpdateStorage> blackboardCallbacks_SecondPass; 
+    DynArray<IUpdatableSystem::UpdateStorage> preRenderUpdate; 
+    DynArray<IUpdatableSystem::UpdateStorage> multiplayer_CaptureStateSnapshots;
+    DynArray<IUpdatableSystem::UpdateStorage> unknown; 
+}
+
 } // namespace RED4ext
