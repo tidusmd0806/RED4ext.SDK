2881dd5d853fb6465ebcce5d07b1bbb0f6fb1c66
Jack Humbert <jack.humb@gmail.com>
2023-05-02 16:20:16 -0400

    change to wchar_t

--- include/RED4ext/Api/v0/Scripts.hpp
+++ include/RED4ext/Api/v0/Scripts.hpp
@@ -32,6 +32,6 @@ struct Scripts
      *     return true;
      * }
      */
-    bool (*Add)(PluginHandle aHandle, const char *aPath);
+    bool (*Add)(PluginHandle aHandle, const wchar_t *aPath);
 };
 } // namespace RED4ext::v0
